92b9b431cc35064b0a975facc54d18a7a6f77422
jim3 <j.x.casey@protonmail.com>
2021-03-07 22:24:06 -0500

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,16 +1,4 @@
-/*
-Write a class named Car that has the following fields:
-# yearModel: The yearModel is an int that holds the car's year model.
-# make: The make field references a String object that holdes the make of the car. For example, Ford.
-# speed: The speed field is an int that holds the car's current speed.
-
-In addition, the class should have the following constructor and other methods:
-# Constructor: The constructor should accept the car's year model and make as arguments. These values
-should be assigned to the object's yearModel and make fields. The constructor should assign 0 to the speed field.
-# Accessors: Appropriate accessor methods should get the values stored in an object's yearModel, make, and speed fields.
-# accelerate: The accelerate mthod should add 5 to the speed field each time it is called.
-# brake: The brake method should subtract 5 from the speed field each time it is called.
-*/
+// Car class exercise
 
 public class Car {
 	private int yearModel;
